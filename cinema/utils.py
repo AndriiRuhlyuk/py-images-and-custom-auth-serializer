f2f1f7d1e16cc8f645396f07856c85c29afce467
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-07-27 20:17:36 +0200

    fix utils

--- cinema/utils.py
+++ cinema/utils.py
@@ -3,7 +3,7 @@ import pathlib
 import uuid
 
 
-def movie_image_path(movie: "Movie", filename: str) -> pathlib.Path:
-    filename = (f"{slugify(movie.title)}-{uuid.uuid4()}"
+def movie_image_path(instance, filename: str) -> pathlib.Path:
+    filename = (f"{slugify(instance.title)}-{uuid.uuid4()}"
                 + pathlib.Path(filename).suffix)
     return pathlib.Path("upload/movies/") / pathlib.Path(filename)
