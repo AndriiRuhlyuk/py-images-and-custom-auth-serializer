df379fece128860bdeca102e9e4ab7f84fc0e61e
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-07-27 20:33:56 +0200

    movie serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -31,6 +31,8 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(read_only=True)
+
     class Meta:
         model = Movie
         fields = (
@@ -42,10 +44,9 @@ class MovieSerializer(serializers.ModelSerializer):
             "genres",
             "actors"
         )
-        read_only_fields = ("id", "image")
 
 
-class MovieImageSerializer(MovieSerializer):
+class MovieImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ("id", "image")
